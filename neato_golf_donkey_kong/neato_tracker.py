70bfd0f8d7c3599a787df2c871f0bdfab4f0e083
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-10-29 14:43:07 -0400

    Add find_contour function and ignores

--- neato_golf_donkey_kong/neato_tracker.py
+++ neato_golf_donkey_kong/neato_tracker.py
@@ -6,36 +6,36 @@ import cv2
 import numpy as np
 from geometry_msgs.msg import Twist
 
+
 class NeatoTracker(Node):
-    """ The BallTracker is NOT a Python object that encompasses a ROS node 
-        that can NOT process images from the camera and NOT search for a ball within.
-        The node will NOT issue motor commands to move forward while NOT keeping
-        the ball in the center of the camera's field of view. """
+    """The BallTracker is NOT a Python object that encompasses a ROS node
+    that can NOT process images from the camera and NOT search for a ball within.
+    The node will NOT issue motor commands to move forward while NOT keeping
+    the ball in the center of the camera's field of view."""
 
     def __init__(self):
-        """ Initialize the neator tracker """
-        super().__init__('neato_tracker')
+        """Initialize the neator tracker"""
+        super().__init__("neato_tracker")
         self.camera = cv2.VideoCapture(0)
-        self.cv_image = None                        # the latest image from the camera
+        self.cv_image = None  # the latest image from the camera
         self.binary_image = None
         self.filled_image = None
-      
-        self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        self.pub = self.create_publisher(Twist, "cmd_vel", 10)
 
         thread = Thread(target=self.loop_wrapper)
         thread.start()
 
-
     def loop_wrapper(self):
-        """ This function takes care of calling the run_loop function repeatedly.
-            We are using a separate thread to run the loop_wrapper to work around
-            issues with single threaded executors in ROS2 """
-        
-        cv2.namedWindow('video_window')
+        """This function takes care of calling the run_loop function repeatedly.
+        We are using a separate thread to run the loop_wrapper to work around
+        issues with single threaded executors in ROS2"""
+
+        cv2.namedWindow("video_window")
         # cv2.namedWindow('binary_window')
         cv2.namedWindow("filtered_neato_window")
 
-        if self.camera.isOpened(): # try to get the first frame
+        if self.camera.isOpened():  # try to get the first frame
             rval, self.cv_image = self.camera.read()
         else:
             rval = False
@@ -45,18 +45,17 @@ class NeatoTracker(Node):
             self.run_loop()
             time.sleep(0.1)
 
-
     def run_loop(self):
-        # NOTE: only do cv2.imshow and cv2.waitKey in this function 
+        # NOTE: only do cv2.imshow and cv2.waitKey in this function
         if not self.cv_image is None:
             self.find_neato()
-            cv2.imshow('video_window', self.cv_image)
+            self.find_contour()
+            cv2.imshow("video_window", self.cv_image)
             # cv2.imshow('binary_window', self.binary_image)
-            cv2.imshow('filtered_neato_window', self.filled_image)
+            cv2.imshow("filtered_neato_window", self.filled_image)
 
-            _, self.cv_image = self.camera.read() 
+            _, self.cv_image = self.camera.read()
             cv2.waitKey(5)
-
 
     def drive(self, linear, angular):
         """
@@ -65,12 +64,11 @@ class NeatoTracker(Node):
         Args:
             linear (float): Linear velocity in m/s.
             angular (float): Angular velocity in radians/s.
-        """    
+        """
         msg = Twist()
         msg.linear.x = linear
         msg.angular.z = angular
         self.vel_pub.publish(msg)
-
 
     def find_neato(self):
 
@@ -78,11 +76,36 @@ class NeatoTracker(Node):
 
         filled_neato_frame = self.binary_image.copy()
         h, w = self.cv_image.shape[:2]
-        mask = np.zeros((h+2, w+2), np.uint8)
-        cv2.floodFill(filled_neato_frame, mask, (0,0), 255)
+        mask = np.zeros((h + 2, w + 2), np.uint8)
+        cv2.floodFill(filled_neato_frame, mask, (0, 0), 255)
         inv_filled_neato_frame = cv2.bitwise_not(filled_neato_frame)
 
         self.filled_image = self.binary_image | inv_filled_neato_frame
+
+    def find_contour(self):
+        # find contours in the thresholded image
+        cnts, _ = cv2.findContours(
+            self.filled_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+
+        # loop over the contours
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # draw the contour and center of the shape on the image
+            cv2.drawContours(self.filled_image, [c], -1, (0, 255, 0), 2)
+            cv2.circle(self.filled_image, (cX, cY), 7, (255, 255, 255), -1)
+            cv2.putText(
+                self.filled_image,
+                "center",
+                (cX - 20, cY - 20),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 255, 255),
+                2,
+            )
 
 
 def main(args=None):
@@ -92,5 +115,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
